75bf64e1d89c05c16765eeae294e951ede9b8ad0
chocho77 <chavmom@gmail.com>
2024-12-09 21:11:46 +0200

    modified:   main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from ui import print_user_options, take_user_input
+
+
 if __name__ == '__main__':
     print("Automotive inventory")
     print_user_options()
     user_input = take_user_input()
-    
+
 
