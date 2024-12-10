7f3c296591e275a885190a36c9d9f318cc7af542
chocho77 <chavmom@gmail.com>
2024-12-10 07:56:16 +0200

    modified:   __pycache__/vehicle_management.cpython-312.pyc 	new file:   database.py 	modified:   main.py 	modified:   vehicle_management.py

--- vehicle_management.py
+++ vehicle_management.py
@@ -1,3 +1,6 @@
+from sys import exit
+
+
 MAIN_MENU_OPTIONS =(
         "Add vehicle to inventory",
         "Delete vehicle from inventory",
@@ -6,3 +9,11 @@ MAIN_MENU_OPTIONS =(
         "Export current inventory",
         "Quit"
 )
+
+def map_user_input(user_input:int):
+    if user_input == 1:
+        pass
+    elif user_input == 6:
+        exit(0)
+    else:
+        raise RuntimeError(f"Unknown main menu option {user_input}")
