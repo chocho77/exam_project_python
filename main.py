3abc741a9e528db4f580a83e7b4e2acd9e6919e4
chocho77 <chavmom@gmail.com>
2024-12-10 06:33:26 +0200

    Changes to be committed: 	modified:   main.py 	modified:   validation.py

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from ui import print_user_options, take_user_input
-
+from validation import is_main_menu_input_valid
 
 if __name__ == '__main__':
     print("Automotive inventory")
     print_user_options()
     user_input = take_user_input()
+    if is_main_menu_input_valid(user_input):
+        print(f"User chose {user_input}")
+    else:
+        print("Wrong option")
+    
 
 
