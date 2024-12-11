1b24b40b4f74f20ba026eed7b54e8642e07e2a38
chocho77 <chavmom@gmail.com>
2024-12-11 10:30:35 +0200

    modified:   __pycache__/vehicle_management.cpython-311.pyc 	modified:   vehicle_management.py

--- vehicle_management.py
+++ vehicle_management.py
@@ -1,6 +1,6 @@
 from sys import exit
 from typing import Tuple
-from database import add_vehicle_to_db,get_database
+from database import add_vehicle_to_db,get_database,delete_vehicle_from_db
 
 
 MAIN_MENU_OPTIONS =(
@@ -17,7 +17,16 @@ def map_user_input(user_input:int):
         vehicle_data = take_user_add_vehicle_input()
         #validate
         add_vehicle_to_db(*vehicle_data)
-        print(get_database())
+        print("Vehicle added successfully.")
+    elif user_input == 2:
+        _id = take_user_delete_vehicle_input
+        #validate
+        delete_vehicle_from_db(int(_id))
+        print("Vehicle deleted successfully.")
+    elif user_input == 3:
+        print_inventory()
+        pass
+
 
     elif user_input == 6:
         exit(0)
@@ -32,3 +41,12 @@ def take_user_add_vehicle_input() -> Tuple[str, str, str, str, str]:
     range = input()
 
     return make, model, year, color, range
+
+def take_user_delete_vehicle_input():
+    return input("Enter id")
+
+def print_inventory():
+    print("Car details:")
+    for i, car_data in enumerate(get_database(), start=1):
+        print(f"#{i} {car_data[0]} {car_data[1]} {car_data[2]} {car_data[3]} {car_data[4]}")
+    print()
